ab35565bff566df4c565894af6967f77daaea37d
Leda <ledathemis@protonmail.com>
2022-08-23 11:31:55 +0000

    feat: typescript Post model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,12 +1,22 @@
 import { Schema, Types, model } from 'mongoose';
 
-const PostSchema = new Schema(
+interface IPost {
+    content: string;
+    photos: Types.ObjectId[];
+    author: Types.ObjectId;
+    comments: Types.ObjectId[];
+    likes: Types.ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const PostSchema = new Schema<IPost>(
     {
         content: { type: String, required: true },
         photos: [{ type: String, required: true }],
-        author: { type: Types.ObjectId, ref: 'User', required: true },
-        comments: [{ type: Types.ObjectId, ref: 'Comment', required: true }],
-        likes: [{ type: Types.ObjectId, ref: 'User', required: true }],
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        comments: [{ type: Schema.Types.ObjectId, ref: 'Comment', required: true }],
+        likes: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
     },
     {
         timestamps: true,
