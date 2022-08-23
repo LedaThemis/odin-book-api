4358cf4b0fa654af024ca77e0a7c6e980ee7edc1
Leda <ledathemis@protonmail.com>
2022-08-23 11:34:47 +0000

    feat: typescript User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,23 @@
 import { Schema, Types, model } from 'mongoose';
 
-const UserSchema = new Schema(
+interface IUser {
+    displayName: string;
+    photoURL: string;
+    friends: Types.ObjectId[];
+    friendRequests: Types.ObjectId[];
+    accessToken: string;
+    refreshToken: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const UserSchema = new Schema<IUser>(
     {
         displayName: { type: String, required: true },
         photoURL: { type: String, required: true },
-        friends: [{ type: Types.ObjectId, ref: 'User', required: true }],
+        friends: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
         friendRequests: [
-            { type: Types.ObjectId, ref: 'FriendRequest', required: true },
+            { type: Schema.Types.ObjectId, ref: 'FriendRequest', required: true },
         ],
         accessToken: { type: String, required: true },
         refreshToken: { type: String, required: true },
